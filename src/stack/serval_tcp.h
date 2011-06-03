bed8108331fdf0bef0ea40d1d1f78a33b2d4999c
Erik Nordström <enordstr@cs.princeton.edu>
2011-06-03 12:06:07 -0400

    Updates

--- src/stack/serval_tcp.h
+++ src/stack/serval_tcp.h
@@ -740,6 +740,17 @@ static inline void serval_tcp_update_wl(struct serval_tcp_sock *tp, u32 seq)
 	tp->snd_wl1 = seq;
 }
 
+
+static inline __sum16 __serval_tcp_checksum_complete(struct sk_buff *skb)
+{
+	return 0; /* FIXME: __skb_checksum_complete(skb); */
+}
+
+static inline int serval_tcp_checksum_complete(struct sk_buff *skb)
+{
+	return 0; /* FIXME: !skb_csum_unnecessary(skb) &&
+                    __tcp_checksum_complete(skb); */
+}
 /* Prequeue for VJ style copy to user, combined with checksumming. */
 
 static inline void serval_tcp_prequeue_init(struct serval_tcp_sock *tp)
