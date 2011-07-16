99b303b63e8fdab9d3933d943f6874ce7daa8fb8
Erik Nordstrom <enordstr@cs.princeton.edu>
2011-07-15 20:36:35 -0400

    Force use of sock_no_sendpage in tcp_sendpage

--- src/stack/serval_tcp.c
+++ src/stack/serval_tcp.c
@@ -1060,10 +1060,15 @@ int serval_tcp_sendpage(struct sock *sk, struct page *page, int offset,
 {
 	ssize_t res;
 
-	if (!(sk->sk_route_caps & NETIF_F_SG) ||
-	    !(sk->sk_route_caps & NETIF_F_ALL_CSUM))
-		return sock_no_sendpage(sk->sk_socket, page, offset, size,
+        /* Using do_tcp_sendpages requires functioning hardware
+           checksum support, and that doesn't work for Serval
+           headers. Therefore, we must force use of normal sendmsg
+           (called by sock_no_sendpage. */
+	if (1 || !(sk->sk_route_caps & NETIF_F_SG) ||
+	    !(sk->sk_route_caps & NETIF_F_ALL_CSUM)) {
+                return sock_no_sendpage(sk->sk_socket, page, offset, size,
 					flags);
+        }
 
 	lock_sock(sk);
 	TCP_CHECK_TIMER(sk);
@@ -1881,6 +1886,7 @@ void __serval_tcp_v4_send_check(struct sk_buff *skb,
         if (!checksum_mode) {
                 /* Force checksum calculation by protocol */
                 skb->ip_summed = CHECKSUM_NONE;
+                LOG_INF("checksumming packet\n");
                 th->check = serval_tcp_v4_check(len, saddr, daddr,
                                                 csum_partial(th,
                                                              len,
