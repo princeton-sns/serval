23019c266354d886260f9bb285438a5dac4759f3
Rob Kiefer <rkiefer@cs.princeton.edu>
2012-04-02 20:45:52 -0400

    More dynamic script for iptables rules

--- src/translator/transhttp.bash
+++ src/translator/transhttp.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
-iptables -t nat -A OUTPUT -p tcp --destination 0.0.0.0/0.0.0.0 --dport 80 -m tcp --syn -j REDIRECT --to-ports 8080
-iptables -t nat -A OUTPUT -p tcp --destination 0.0.0.0/0.0.0.0 --dport 443 -m tcp --syn -j REDIRECT --to-ports 8080
+PORT="8080"
+if [ "$1" != "" ]; then
+    PORT=$1
+fi
+echo "port $PORT"
+iptables -t nat -A OUTPUT -p tcp --dport 80 -m tcp --syn -j REDIRECT --to-ports $PORT
+iptables -t nat -A OUTPUT -p tcp --dport 443 -m tcp --syn -j REDIRECT --to-ports $PORT
