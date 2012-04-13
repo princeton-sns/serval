201d7a3a8b318d757d7bcfd72f4b7961840a8c52
Rob Kiefer <rkiefer@cs.princeton.edu>
2012-04-13 14:47:28 -0400

    Updated the scripts a bit.

--- src/translator/transhttp.bash
+++ src/translator/transhttp.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
+iptables -F
 iptables -t nat -F
-iptables -t nat -A OUTPUT -p tcp -m tcp --syn -j REDIRECT --to-ports 8080
-iptables -A FORWARD -s 192.168.25.0/255.255.255.0 -p udp --dport 53 -j ACCEPT
-iptables -A FORWARD -s 192.168.25.0/255.255.255.0 -j DROP
-iptables -t nat -A POSTROUTING -p udp -j MASQUERADE
-
+iptables -t nat -A OUTPUT -p tcp --dport 80 -m tcp --syn -j REDIRECT --to-ports 8080
+iptables -t nat -A OUTPUT -p tcp --dport 443 -m tcp --sync -j REDIRECT --to-ports 8080
+iptables -A FORWARD -s 192.168.25.0/255.255.255.0 -p tcp --dport 80 -j DROP
+iptables -A FORWARD -s 192.168.25.0/255.255.255.0 -p tcp --dport 443 -j DROP
+iptables -A FORWARD -s 192.168.25.0/255.255.255.0 -j ACCEPT
+iptables -t nat -A POSTROUTING -j MASQUERADE
