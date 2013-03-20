685201a3db370306947bcbd70d8810c3e6fd047f
Erik Nordström <erik.nordstrom@gmail.com>
2013-03-20 15:31:36 -0400

    Compile fixes for Android

--- android/Serval/src/org/servalarch/serval/AppHostCtrl.java
+++ android/Serval/src/org/servalarch/serval/AppHostCtrl.java
@@ -51,18 +51,12 @@ public class AppHostCtrl {
 			final String ipStr, int op) {
 		ServiceID sid;
 		InetAddress addr = null;
-		int prefixBits = 0;
 		int type = HostCtrl.SERVICE_RULE_FORWARD;
 
 		if (hc == null)
 			return;
-
-		String res[] = serviceStr.split(":");
 		
-		if (res.length == 2)
-			prefixBits = Integer.parseInt(res[1]);
-		
-		sid = AppHostCtrl.createServiceID(res[0]);
+		sid = AppHostCtrl.createServiceID(serviceStr);
 
 		if (sid == null) {
 			Toast t = Toast.makeText(context, "Not a valid serviceID",
